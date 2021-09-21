e23181aed0df0a2c04454f5d38ace59af70c70e7
Romy <romeo32121@hotmail.com>
2021-09-21 18:00:54 +0800

    Updated dh-graphql-codegen-ios to 0.1.13

--- Formula/dh-graphql-codegen-ios.rb
+++ Formula/dh-graphql-codegen-ios.rb
@@ -1,8 +1,8 @@
 class DhGraphqlCodegenIos < Formula
   desc "Swift GraphQL Code Generator"
   homepage "https://github.com/lRoMYl/dh-graphql-codegen-ios"
-  url "https://github.com/lRoMYl/dh-graphql-codegen-ios/archive/0.1.12.tar.gz"
-  sha256 "0f8ee33af104b4458236c2fa7a1307ff4380b873f4111f50aad3a9b3238bf45d"
+  url "https://github.com/lRoMYl/dh-graphql-codegen-ios/archive/0.1.13.tar.gz"
+  sha256 "2a6c94e6f81e5d95032e7d6a5ad4816dbdff15ee5a09a1089060aa531d8c6353"
   head "https://github.com/lRoMYl/dh-graphql-codegen-ios.git"
 
   depends_on :xcode => :build
