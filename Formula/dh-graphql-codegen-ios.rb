c0f6471cc44d79b06880492c244aaf3673c14739
Romy <romeo32121@hotmail.com>
2021-09-26 16:35:56 +0800

    Fix brew spec

--- Formula/dh-graphql-codegen-ios.rb
+++ Formula/dh-graphql-codegen-ios.rb
@@ -1,7 +1,7 @@
 class DhGraphqlCodegenIos < Formula
   desc "Swift GraphQL Code Generator"
   homepage "https://github.com/lRoMYl/dh-graphql-codegen-ios"
-  url "https://github.com/lRoMYl/dh-graphql-codegen-ios/archive/0.2.00.tar.gz"
+  url "https://github.com/lRoMYl/dh-graphql-codegen-ios/archive/0.2.0.tar.gz"
   sha256 "614d0d474448b20240e202ee39d0dbde28af01e15be6699587db800036e925d9"
   head "https://github.com/lRoMYl/dh-graphql-codegen-ios.git"
 
