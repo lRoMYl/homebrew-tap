6971db8ea09d57b1cd13cfa7e4eb154d58aa180c
Romy <romeo32121@hotmail.com>
2022-01-06 14:47:52 +0800

    Added private strategy

--- Formula/dh-graphql-codegen.rb
+++ Formula/dh-graphql-codegen.rb
@@ -1,7 +1,10 @@
+require "formula"
+require_relative "../lib/private_strategy"
+
 class DhGraphqlCodegen < Formula
   desc "Swift GraphQL Code Generator"
   homepage "https://github.com/lRoMYl/dh-graphql-codegen-ios"
-  url "https://github.com/lRoMYl/dh-graphql-codegen-ios/archive/0.5.3.tar.gz"
+  url "https://github.com/lRoMYl/dh-graphql-codegen-ios/archive/0.5.3.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "c2037f4f06f5d0af6142ae9389a158d68655fea0baa4cb074566051ed574bd43"
   head "https://github.com/lRoMYl/dh-graphql-codegen-ios.git"
 
