a1e2963debf0cf87870a3389d323c58bb94bfeec
Romy <romeo32121@hotmail.com>
2022-05-29 21:14:01 +0800

    Updated ruby spec

--- Formula/swift-graphql-codegen.rb
+++ Formula/swift-graphql-codegen.rb
@@ -1,9 +1,7 @@
-require_relative "../lib/private_strategy"
-
 class SwiftGraphqlCodegen < Formula
   desc "Swift GraphQL Code Generator"
   homepage "https://github.com/lRoMYl/swift-graphql-codegen"
-  url "https://github.com/lRoMYl/swift-graphql-codegen/archive/0.8.3.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy
+  url "https://github.com/lRoMYl/swift-graphql-codegen/archive/0.8.3.tar.gz"
   sha256 "d02db93342b82aff32a8bf03b2ad638ee0fc7d6e73a2084ee339ddd10065aa33"
   head "https://github.com/lRoMYl/swift-graphql-codegen.git"
 
