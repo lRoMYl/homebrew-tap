a4bf9b0c8ae78450d35c55c3a60758f79688cc4d
Romy <romeo32121@hotmail.com>
2022-05-29 21:12:22 +0800

    Renamed ruby

---
+++ Formula/swift-graphql-codegen.rb
@@ -0,0 +1,16 @@
+require_relative "../lib/private_strategy"
+
+class SwiftGraphqlCodegen < Formula
+  desc "Swift GraphQL Code Generator"
+  homepage "https://github.com/lRoMYl/swift-graphql-codegen"
+  url "https://github.com/lRoMYl/swift-graphql-codegen/archive/0.8.3.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy
+  sha256 "d02db93342b82aff32a8bf03b2ad638ee0fc7d6e73a2084ee339ddd10065aa33"
+  head "https://github.com/lRoMYl/swift-graphql-codegen.git"
+
+  depends_on :xcode => :build
+  depends_on :macos
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+end
