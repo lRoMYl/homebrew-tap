2cb208aea437361d018fcfdfbb0e00a5ea526ed6
Romy <5739692+lRoMYl@users.noreply.github.com>
2023-07-04 15:42:50 +0800

    Update swift-graphql-codegen.rb

--- Formula/swift-graphql-codegen.rb
+++ Formula/swift-graphql-codegen.rb
@@ -1,7 +1,7 @@
 class SwiftGraphqlCodegen < Formula
   desc "Swift GraphQL Code Generator"
   homepage "https://github.com/lRoMYl/swift-graphql-codegen"
-  url "https://github.com/lRoMYl/swift-graphql-codegen/archive/alpha-0.10.3.tar.gz"
+  url "https://github.com/lRoMYl/swift-graphql-codegen/archive/alpha-0.11.0.tar.gz"
   sha256 "5ef34c38abcf1c8f8e8adf27ab0b0f5de1a1ce914b64590a86a19b77b3602ef5"
   head "https://github.com/lRoMYl/swift-graphql-codegen.git"
 
