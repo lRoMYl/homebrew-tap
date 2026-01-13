144163b54b3a667a80d72929447a3c8d35eb34db
lRoMYl <romy.cheah@gmail.com>
2026-01-13 23:30:04 +0800

    Updated to 0.0.2

--- Formula/xcode-helper.rb
+++ Formula/xcode-helper.rb
@@ -1,8 +1,8 @@
 class XcodeHelper < Formula
   desc "Helper tools for Xcode project management"
   homepage "https://github.com/lRoMYl/xcode-helper"
-  url "https://github.com/lRoMYl/xcode-helper/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "19120a6d6f6d64a1ab80f0d04ee023a86cf9c3ae652096fb54aced97c70de343"
+  url "https://github.com/lRoMYl/xcode-helper/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "e58f39aa751e7eae31426f9edad82d2e8d27f4c215280b6e4eee2fb6f71d3af6"
   head "https://github.com/lRoMYl/xcode-helper.git", branch: "main"
 
   depends_on :xcode => :build
